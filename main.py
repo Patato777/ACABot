2fa691300b965f8fb27b2b62c55d63048635fc6b
Patato777 <jean.sma@free.fr>
2021-10-01 10:17:28 +0200

    Replace strings with others

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import discord
 
+REPLACE = {"putain": "ofan de chichoune", "fils de pute": "fils de flic"}
+
 bot = discord.Client()
+
+
+def replace(text, old, new):
+    send = str()
+    prev = -len(old)
+    for i, c in enumerate(text):
+        if text[i:].lower().startswith(old):
+            send += new
+            prev = i
+        elif prev + len(old) - 1 < i:
+            send += c
+    return send
 
 
 @bot.event
@@ -12,16 +26,10 @@ async def on_message(msg):
             await msg.channel.send(msg.content.lower().split("dy")[-1])
         elif "du coup" in msg.content.lower():
             await msg.channel.send('Non, pas du coup, non')
-        elif "putain" in msg.content.lower():
-            send = str()
-            prev = -6
-            for i, c in enumerate(msg.content):
-                if msg.content[i:].lower().startswith('putain'):
-                    send += "ofan de chichoune"
-                    prev = i
-                elif prev + 5 < i:
-                    send += c
-            await msg.channel.send("*" + send)
+        else:
+            for key in REPLACE.keys():
+                if key in msg.content.lower():
+                    await msg.channel.send("*" + replace(msg.content, key, REPLACE[key]))
         test_koi = ''.join([c for c in msg.content.lower() if c.isalpha()])
         if test_koi.endswith("quoi") or test_koi.endswith("koi") or (
                 test_koi[-1] in ['t', 'e', 'p', 's', 'd', 'h'] and (
